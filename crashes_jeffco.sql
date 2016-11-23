5118505bea4fd2259427b6d1112c82bddf674695
Spencer Gardner <spencergardner@gmail.com>
2016-11-22 21:41:49 -0600

    add jefferson county

---
+++ crashes_jeffco.sql
@@ -0,0 +1,67 @@
+-- Must be run as superuser.
+-- Assumes CSV has header rows and is separated by pipe.
+-- File must be copied to the server and given read permission for all users
+DROP TABLE IF EXISTS received.crashes_jeffco;
+CREATE TABLE received.crashes_jeffco (
+    hwy TEXT,
+    mp TEXT,
+    "date" TEXT,
+    "time" TEXT,
+    severity TEXT,
+    agencyname TEXT,
+    report_id TEXT,
+    location TEXT,
+    road_desc TEXT,
+    vehicles TEXT,
+    contour TEXT,
+    condition TEXT,
+    lighting TEXT,
+    weather TEXT,
+    ramp TEXT,
+    acctype TEXT,
+    dir_1 TEXT,
+    vehicle_1 TEXT,
+    driver_1 TEXT,
+    factor_1 TEXT,
+    speed_1 TEXT,
+    veh_move_1 TEXT,
+    dir_2 TEXT,
+    vehicle_2 TEXT,
+    driver_2 TEXT,
+    factor_2 TEXT,
+    speed_2 TEXT,
+    veh_move_2 TEXT,
+    loc_01 TEXT,
+    link TEXT,
+    loc_02 TEXT
+);
+ALTER TABLE received.crashes_jeffco OWNER TO gis;
+
+-- copy data in
+COPY received.crashes_jeffco FROM '/home/sgardner/listing-jeffco(2008-15).csv' DELIMITER '|' HEADER QUOTE '"' CSV;
+
+-- remove the stuff we don't care about
+DELETE FROM received.crashes_jeffco
+WHERE   NOT hwy = '0095A';
+
+-- add columns
+ALTER TABLE received.crashes_jeffco ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE received.crashes_jeffco ADD COLUMN tdg_id VARCHAR(36) DEFAULT (uuid_generate_v4())::TEXT;
+ALTER TABLE received.crashes_jeffco ADD COLUMN road_id1 INTEGER;
+ALTER TABLE received.crashes_jeffco ADD COLUMN road_id2 INTEGER;
+ALTER TABLE received.crashes_jeffco ADD COLUMN at_intersection BOOLEAN;
+ALTER TABLE received.crashes_jeffco ADD COLUMN int_id INTEGER;
+ALTER TABLE received.crashes_jeffco ADD COLUMN geom_int geometry(point,2231);
+ALTER TABLE received.crashes_jeffco ADD COLUMN geom_midpoint geometry(point,2231);
+ALTER TABLE received.crashes_jeffco ADD COLUMN geom_exact geometry(point,2231);
+
+-- update column types
+ALTER TABLE received.crashes_jeffco ALTER COLUMN mp TYPE FLOAT USING mp::FLOAT;
+ALTER TABLE received.crashes_jeffco ALTER COLUMN "date" TYPE DATE USING "date"::DATE;
+ALTER TABLE received.crashes_jeffco ALTER COLUMN "time" TYPE INTEGER USING "time"::INTEGER;
+ALTER TABLE received.crashes_jeffco ALTER COLUMN vehicles TYPE INTEGER USING vehicles::INTEGER;
+
+-- remove crashes pre 2011 or post 2015
+DELETE FROM crashes_jeffco
+WHERE   EXTRACT(YEAR FROM "date") < 2011
+OR      EXTRACT(YEAR FROM "date") > 2015;
--- process_crashes.sql
+++ process_crashes.sql
@@ -52,3 +52,52 @@ ANALYZE crashes_ped (int_id);
 ANALYZE crashes_veh (int_id);
 ANALYZE crashes_bike1 (int_id);
 ANALYZE crashes_bike2 (int_id);
+
+
+----------------------------
+-- jefferson county crashes
+----------------------------
+-- geom_exact
+UPDATE  crashes_jeffco
+SET     geom_exact = ST_LineInterpolatePoint(
+            ch.geom,
+            (crashes_jeffco.mp - ch.refpt) / (ch.endrefpt - ch.refpt)
+        )
+FROM    cdot_highways ch
+WHERE   ch.route = '095A'
+AND     crashes_jeffco.mp >= ch.refpt
+AND     crashes_jeffco.mp < ch.endrefpt;
+
+CREATE INDEX sidx_crashes_jeffco_geomexact ON crashes_jeffco USING GIST (geom_exact);
+ANALYZE crashes_jeffco (geom_exact);
+
+-- remove crashes from outside denver limits
+DELETE FROM crashes_jeffco
+WHERE   NOT EXISTS (
+            SELECT  1
+            FROM    denver_streets ds
+            WHERE   ST_DWithin(crashes_jeffco.geom_exact,ds.geom,1000)
+        );
+
+-- snap to denver streets
+UPDATE  crashes_jeffco
+SET     geom_exact = ST_ClosestPoint(ds.geom,crashes_jeffco.geom_exact),
+        road_id1 = ds.road_id
+FROM    denver_streets ds
+WHERE   ds.road_id = (
+            SELECT      ds2.road_id
+            FROM        denver_streets ds2
+            ORDER BY    crashes_jeffco.geom_exact <-> ds2.geom ASC
+            LIMIT       1
+        );
+
+-- int_id
+UPDATE  crashes_jeffco
+SET     int_id = (
+            SELECT      int_id
+            FROM        denver_streets_intersections dsi
+            WHERE       legs > 2
+            ORDER BY    crashes_jeffco.geom_exact <-> dsi.geom ASC
+            LIMIT       1
+        )
+WHERE   trim(lower(road_desc)) IN ('at intersection','intersection related');
