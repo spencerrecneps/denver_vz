93ec62746d3e8446814ad59278c584ea48d9a32f
Spencer <spencergardner@gmail.com>
2016-11-29 11:43:45 -0600

    change process to shell script

---
+++ process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+psql -h 192.168.40.225 -U gis -d denver_vz -f denver_streets.sql
+psql -h 192.168.40.225 -U postgres -d denver_vz -f crashes_veh.sql
+psql -h 192.168.40.225 -U postgres -d denver_vz -f crashes_ped.sql
+psql -h 192.168.40.225 -U postgres -d denver_vz -f crashes_bike1.sql
+psql -h 192.168.40.225 -U postgres -d denver_vz -f crashes_bike2.sql
+psql -h 192.168.40.225 -U postgres -d denver_vz -f crashes_jeffco.sql
+psql -h 192.168.40.225 -U gis -d denver_vz -f process_crashes.sql
+psql -h 192.168.40.225 -U gis -d denver_vz -f crash_aggregates.sql
